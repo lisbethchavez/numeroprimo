95c2c0230dd55198cd37c2ea2714b672cb94456e
lisbethchavez <chavezlsibeth639@gmail.com>
2021-06-18 16:44:44 -0500

    Enunciado del Ejercicio

---
+++ cmake-build-debug/CMakeFiles/numeroprimo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numeroprimo.dir/main.cpp.obj"
+  "libnumeroprimo.dll.a"
+  "numeroprimo.exe"
+  "numeroprimo.exe.manifest"
+  "numeroprimo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numeroprimo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//Se ingresa un entero por teclado. Se desea determinar si el numero ingresado es primo o no
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
