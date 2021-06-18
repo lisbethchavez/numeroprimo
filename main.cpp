9e83a4882286cc49639be2dfae99287c21871708
lisbethchavez <chavezlsibeth639@gmail.com>
2021-06-18 17:07:26 -0500

    Determinar si un numero es primo o no

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <stdio.h>
 //Se ingresa un entero por teclado. Se desea determinar si el numero ingresado es primo o no
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    cout<<"Ingrese un numero: ";
+    cin>>num;
+    bool band = true;
+    int i =2;
+    while(band==true && i<num ){
+        if(num%i==0){
+            band=false;
+        }else{
+            i++;
+        }
+    }
+    if(band==true){
+        cout<<"El numero ingresado es primo"<<endl;
+    }else{
+        cout<<"El numero ingresado no es primo"<<endl;
+    }
+    cout<<band<<endl;
+
     return 0;
 }
